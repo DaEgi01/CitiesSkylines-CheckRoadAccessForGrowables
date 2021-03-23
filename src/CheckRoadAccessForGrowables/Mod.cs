701c5046ef595cc92d8abacca7a406705010e14d
egi <daegi01@gmail.com>
2021-03-23 22:31:24 +0100

    show option button only during gameplay and tell players if the process is completed.

--- src/CheckRoadAccessForGrowables/Mod.cs
+++ src/CheckRoadAccessForGrowables/Mod.cs
@@ -1,6 +1,9 @@
-﻿using Harmony;
+﻿using ColossalFramework.Threading;
+using ColossalFramework.UI;
+using Harmony;
 using ICities;
 using System.Reflection;
+using UnityEngine.SceneManagement;
 
 namespace CheckRoadAccessForGrowables
 {
@@ -29,17 +32,42 @@ namespace CheckRoadAccessForGrowables
 
 		public void OnSettingsUI(UIHelperBase helper)
 		{
-			helper.AddButton("Recheck road access", () => {
-				SimulationManager.instance.AddAction(() =>
-				{
-					var buildings = BuildingManager.instance.m_buildings.m_buffer;
-					for (ushort i = 0; i < buildings.Length; i++)
+			if (SceneManager.GetActiveScene().name == "Game")
+			{
+				helper.AddGroup(Name)
+					.AddButton("Recheck road access", () =>
 					{
-						BuildingInfo info = buildings[i].Info;
-						info.m_buildingAI.CheckRoadAccess(i, ref buildings[i]);
-					}
-				});
-			});
+						SimulationManager.instance.AddAction(() =>
+						{
+							var buildings = BuildingManager.instance.m_buildings;
+							for (ushort i = 0; i < buildings.m_size; i++)
+							{
+								var building = buildings.m_buffer[i];
+								if (building.m_flags == Building.Flags.None)
+								{
+									continue;
+								}
+
+								building.Info.m_buildingAI.CheckRoadAccess(i, ref building);
+							}
+
+							SimulationManager.instance.m_ThreadingWrapper.QueueMainThread(() =>
+							{
+								DebugOutputPanel.AddMessage(
+									ColossalFramework.Plugins.PluginManager.MessageType.Warning,
+									"Recheck road access completed. Look for 'No Road Access' icons on your map."
+								);
+							});
+						});
+					});
+			}
+			else
+			{
+				var mainGroupHelper = helper.AddGroup(Name) as UIHelper;
+				var mainPanel = mainGroupHelper.self as UIPanel;
+				var mainLabel = mainPanel.AddUIComponent<UILabel>();
+				mainLabel.text = "This mod can only be used during gameplay.";
+			}
 		}
 	}
 }
